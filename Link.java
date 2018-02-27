ce85c5433bab21d98d756d5e63c1da091e23c105
Joshua Johnston <36349396+JJohnston89@users.noreply.github.com>
2018-02-26 22:55:00 -0500

    Create Link.java

---
+++ Link.java
@@ -0,0 +1,13 @@
+class Link{
+   
+   public int iData;              // data item
+   public Link next;              // next link in list
+// -------------------------------------------------------------
+   public Link(int number)           // constructor
+      { iData = number; }
+// -------------------------------------------------------------
+   public void displayLink()      // display ourself
+      { System.out.print(iData + " "); }
+// -------------------------------------------------------------
+}  end class Link
+////////////////////////////////////////////////////////////////
